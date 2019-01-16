391f63663781ba820ae601af0701d2dce6ced85d
phantombreaker23 <42500584+phantombreaker23@users.noreply.github.com>
2019-01-16 18:07:23 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,71 +1,90 @@
-package com.edu4java.minitennis6;
+package TennisSimple;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.event.KeyEvent;
+import java.event.KeyListener;
 import java.awt.RenderingHints;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
-public class Game extends JPanel {
-
-	Ball ball = new Ball(this);
-	Racquet racquet = new Racquet(this);
-
+public class Game extends JPanel{
+	
+	Ball ball = new ball(this);
+	Board board = new board(this);
+	
 	public Game() {
-		addKeyListener(new KeyListener() {
+		KeyListener listener = new KeyListener() {
 			@Override
 			public void keyTyped(KeyEvent e) {
 			}
 
 			@Override
-			public void keyReleased(KeyEvent e) {
-				racquet.keyReleased(e);
+			public void keyPressed(KeyEvent e) {
+				board.keyPressed();
 			}
 
 			@Override
-			public void keyPressed(KeyEvent e) {
-				racquet.keyPressed(e);
+			public void keyReleased(KeyEvent e) {
+				board.keyPressed();
 			}
-		});
+		};
 		setFocusable(true);
 	}
 	
-	private void move() {
+	public move() {
 		ball.move();
-		racquet.move();
+		board.move();
 	}
-
-	@Override
+	
+	private void moveBall() {
+		// xa = 1: go right xa = -1: go left  ya = 1 go down ya = -1: go up
+		if (x + xa < 0) {
+			xa = 1;
+		}
+		if(x + xa > getWidth() - 30) {
+			xa = -1;
+		}
+		if (y + ya < 0) {
+			ya = 1;
+		}
+		if (y + ya > getHeight() - 30); {
+			ya = -1;
+		}
+		
+		 x = x + 1;
+		 y = y + 1;		
+	}
+	
+	@Override 
 	public void paint(Graphics g) {
-		super.paint(g);
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-				RenderingHints.VALUE_ANTIALIAS_ON);
-		ball.paint(g2d);
-		racquet.paint(g2d);
+			super.paint(g);
+			Graphics2D g2d = (Graphics2D) g;
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+			ball.repaint();
+			board.repaint();
 	}
 	
 	public void gameOver() {
-		JOptionPane.showMessageDialog(this, "Game Over", "Game Over", JOptionPane.YES_NO_OPTION);
+		JOptionPane.showMessageDiaog(this, "Game Over, You Suck","Game Over, You Suck", JOptionPane.YES_NO_OPTION);
 		System.exit(ABORT);
 	}
-
+	
 	public static void main(String[] args) throws InterruptedException {
-		JFrame frame = new JFrame("Mini Tennis");
+		JFrame frame = new JFrame("Simple Tennis");
 		Game game = new Game();
 		frame.add(game);
-		frame.setSize(300, 400);
+		frame.setSize(300, 300);
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		while (true) {
-			game.move();
+			game.moveBall();
 			game.repaint();
 			Thread.sleep(10);
 		}
 	}
+
 }
